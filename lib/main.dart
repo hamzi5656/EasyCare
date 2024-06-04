5c59aeba03a26f843e428ef4a18f9b0e2ee83eb4
Naseeba967 <naseebailyas967@gmail.com>
2024-06-04 10:43:24 +0500

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
-import 'package:auth/User/Authentication/userlogin.dart';
-import 'package:auth/User/Home/HomeNavigator.dart';
+
+import 'package:auth/Authentication/userlogin.dart';
 import 'package:auth/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:get/get.dart';
 import 'package:persistent_shopping_cart/persistent_shopping_cart.dart';
 Future<void> main() async {
 await PersistentShoppingCart().init();
@@ -22,7 +23,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
- return MaterialApp(
+ return GetMaterialApp(
      debugShowCheckedModeBanner: false,
      builder: EasyLoading.init(),
       home: 
